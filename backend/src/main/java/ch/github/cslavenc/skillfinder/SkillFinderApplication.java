07e5ab5b39f57019afefd6fc1a3b3079bee0b3a8
e537699 <slaven.cvijetic@sbb.ch>
2021-10-17 16:57:21 +0200

    Start project

---
+++ backend/src/main/java/ch/github/cslavenc/skillfinder/SkillFinderApplication.java
@@ -0,0 +1,13 @@
+package ch.github.cslavenc.skillfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillFinderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ch/github/cslavenc/skillfinder/SkillFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.github.cslavenc.skillfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
